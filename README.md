ad3f39aff746f67cc9e85fdeba1deb2487c93bf2
Venkatesh Pulagam <venkatesh.pulagam@grainger.com>
2025-03-20 10:51:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+## Setup
+
+1. Make sure you have Node JS installed. If you don't have it:
+
+-   [Download it from nodejs.org](https://nodejs.org)
+-   [Install it using NVM ](https://github.com/nvm-sh/nvm)
+-   If you're on Mac, Homebrew is a good option too:
+
+```
+brew install node
+```
+
+2. Clone the repo:
+
+```
+git clone https://github.com/venkatesh-pulagam_wwg
+```
+
+3. Open the project folder:
+
+```
+cd react-tailwindcss-portfolio
+```
+
+4. Install packages and dependencies:
+
+```
+yarn
+```
+
+4. NOTE: If you don't have yarn installed, you can install it globally using npm:
+
+```
+npm install --global yarn
+```
+
+5. Start a local dev server at `http://localhost:3000`:
+
+```
+yarn start
+```
+
+6. ##### Run tests:
+
+```
+yarn test
+```
+
+## Notes
+
+-   Always run `yarn install` after pulling new changes
+-   I'll be constantly updating this repo as I'll be adding more sections to it, so please always check the projects section of this repo to see what tasks are under todo and in progress
+-   Illustrations from [unDraw](https://undraw.co) and [Freepik](https://freepik.com)
+-   Images from [Unsplash](https://unsplash.com)
+-   Feel free to use it as your own portfolio
+-   Contributions are welcome
---
+++ src/components/shared/AppBanner.jsx
@@ -0,0 +1,71 @@
+import useThemeSwitcher from '../../hooks/useThemeSwitcher';
+import { FiArrowDownCircle } from 'react-icons/fi';
+import developerLight from '../../images/developer.svg';
+import developerDark from '../../images/developer-dark.svg';
+import { motion } from 'framer-motion';
+
+const AppBanner = () => {
+	const [activeTheme] = useThemeSwitcher();
+
+	return (
+		<motion.section
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
+			className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-2"
+		>
+			<div className="w-full md:w-1/3 text-left">
+				<motion.h1
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{
+						ease: 'easeInOut',
+						duration: 0.9,
+						delay: 0.1,
+					}}
+					className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
+				>
+					Hi, Iam Venkatesh
+				</motion.h1>
+				<motion.p
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{
+						ease: 'easeInOut',
+						duration: 0.9,
+						delay: 0.2,
+					}}
+					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
+				>
+					A Full-Stack Developer & Design Enthusiast
+				</motion.p>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{
+						ease: 'easeInOut',
+						duration: 0.9,
+						delay: 0.3,
+					}}
+					className="flex justify-center sm:block"
+				>
+				</motion.div>
+			</div>
+			<motion.div
+				initial={{ opacity: 0, y: -180 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
+				className="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0"
+			>
+				<img
+					src={
+						activeTheme === 'dark' ? developerLight : developerDark
+					}
+					alt="Developer"
+				/>
+			</motion.div>
+		</motion.section>
+	);
+};
+
+export default AppBanner;
